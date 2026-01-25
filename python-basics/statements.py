dff5471d5f5a529ec7c7d558a857df9c0025704c
Didar <didar070819@gmail.com>
2026-01-25 16:36:14 +0500

    Try it Yourself from Python syntax => statements

---
+++ python-basics/statements.py
@@ -0,0 +1,9 @@
+print("Python is fun!")
+
+#print("Hello World!")
+#print("Have a good day.")
+#print("Learning Python is fun!") #The statements are executed one by one, in the same order as they are written
+
+#print("Hello"); print("How are you?"); print("Bye bye!") # You can write multiple statements on one line by separating them with ; but this is rarely used because it makes it hard to read
+
+#print("Python is fun!") print("Really!") #However, if you put two statements on the same line without a separator (newline or ;), Python will give an error
