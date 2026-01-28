4ce1b4d5055b0f2fbc99a9079835f5ae079a0278
Didar <didar070819@gmail.com>
2026-01-28 10:32:22 +0500

    Try it Yourself from python Beriables topic

---
+++ python-basics/variables.py
@@ -0,0 +1,23 @@
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#x = str(3)    # x will be '3'
+#y = int(3)    # y will be 3
+#z = float(3)  # z will be 3.0
+
+"""
+myvar = "John" #legal variable name
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+2myvar = "John" # illegal variable name
+my-var = "John"
+my var = "John"
+"""
+#x, y, z = "Orange", "Banana", "Cherry" #Python allows you to assign values to multiple variables in one line
+#print(x) 
+#print(y)
+#print(z)
