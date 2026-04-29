c4aae4aa3b72d2c0a98bb6a47fe2826b8b459d4b
Didar <didar070819@gmail.com>
2026-04-29 09:58:46 +0500

    Tsis defence materials

---
+++ TSIS1/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "dbname": "phonebook_db",
+    "user": "postgres",
+    "password": "didar132",
+    "port": "5432"
+}
---
+++ TSIS4/config.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+SETTINGS_FILE = 'settings.json'
+
+DEFAULT_SETTINGS = {
+    "snake_color": (0, 255, 0), 
+    "grid_overlay": False,
+    "sound": True
+}
+
+def load_settings():
+    if not os.path.exists(SETTINGS_FILE):
+        save_settings(DEFAULT_SETTINGS)
+        return DEFAULT_SETTINGS
+    with open(SETTINGS_FILE, 'r') as f:
+        settings = json.load(f)
+        settings['snake_color'] = tuple(settings['snake_color'])
+        return settings
+
+def save_settings(settings):
+    with open(SETTINGS_FILE, 'w') as f:
+        json.dump(settings, f, indent=4)
+
+CS = 20 
+W, H = 30, 20
+WIDTH, HEIGHT = W * CS, H * CS 
